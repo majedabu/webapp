4ec1c1432cbcfea512223123d9d01ea6ab43d3db
majed <majed.abumoch@gmail.com>
2016-03-21 11:51:09 +0200

    another time

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+function qs(selector) {
+    return document.querySelector(selector);
+}
+
+function qsa(selector) {
+    return document.querySelectorAll(selector);
+}
 
 function updateNote(notification){
 	if(notification !== undefined){
